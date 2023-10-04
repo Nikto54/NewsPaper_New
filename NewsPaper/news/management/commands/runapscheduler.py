4a79df759c1ba7251939db37aa6b09113cc65c6d
bogdanbondar <bondarbm03@gmail.com>
2023-10-04 15:30:26 +0300

    try

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -57,7 +57,7 @@ class Command(BaseCommand):
         # добавляем работу нашему задачнику
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(day_of_week="Wed",hour='13',minute='31'),
+            trigger=CronTrigger(second='*/10'),
             # То же, что и интервал, но задача тригера таким образом более понятна django
             id="my_job",  # уникальный айди
             max_instances=1,
