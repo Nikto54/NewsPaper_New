ae7a3309b2b3b7bbbf8e40010e45945ed9e0696f
bogdanbondar <bondarbm03@gmail.com>
2023-10-06 18:06:50 +0300

    pre-done d10

--- NewsPaper/NewsPaper/celery.py
+++ NewsPaper/NewsPaper/celery.py
@@ -1,9 +1,18 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
 
 app = Celery('NewsPaper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.conf.beat_schedule = {
+    'new_posts_weekly_sending': {
+        'task': 'news.tasks.send_weekly_mail' ,
+        'schedule': crontab(),
+    },
+}
 
 app.autodiscover_tasks()
--- NewsPaper/news/tasks.py
+++ NewsPaper/news/tasks.py
@@ -2,8 +2,10 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
-from .models import PostCategory,Post
+from .models import PostCategory, Post, Category
 from celery import shared_task
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 from NewsPaper import settings
 
@@ -35,4 +37,25 @@ def send_notifications(pk):
     msg.send()
 
 
-
+@shared_task()
+def send_weekly_mail():
+    today = timezone.now()
+    last_week = today - timedelta(days=7)
+    posts = Post.objects.filter(date__gte=last_week)
+    categories = set(posts.values_list('categories_post__title_category', flat=True))
+    subscribers = set(Category.objects.filter(title_category__in=categories).values_list('subscribers__email',flat=True))
+    html_content = render_to_string(
+        'daily_post.html', {
+            'link': settings.SITE_URL + '/news/',
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+    print('ok')
